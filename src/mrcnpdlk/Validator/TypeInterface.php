ed4354fffcbe205732293e196f3775ed8ef40f75
Marcin Pudełek <mrcnpdlk@gmail.com>
2017-09-17 22:24:31 +0200

    copyright

--- src/mrcnpdlk/Validator/TypeInterface.php
+++ src/mrcnpdlk/Validator/TypeInterface.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Validator
+ *
+ * Copyright (c) 2017 pudelek.org.pl
+ *
+ * @license MIT License (MIT)
+ *
+ * For the full copyright and license information, please view source file
+ * that is bundled with this package in the file LICENSE
+ *
+ * @author Marcin Pudełek <marcin@pudelek.org.pl>
+ */
+
 declare (strict_types=1);
 
 namespace mrcnpdlk\Validator;
