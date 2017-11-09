81aaf1cb42e0ff801eec4e775f73def8b844a55e
Marcin Pudełek <mpudelek@widzew.net>
2017-11-09 10:42:21 +0100

    getRegion()

---
+++ src/mrcnpdlk/Validator/Databases/ukeFixedPlan.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Validator
+ *
+ * Copyright (c) 2017 pudelek.org.pl
+ *
+ * @license MIT License (MIT)
+ *
+ * For the full copyright and license information, please view source file
+ * that is bundled with this package in the file LICENSE
+ *
+ * @author  Marcin Pudełek <marcin@pudelek.org.pl>
+ */
+
+/**
+ * Created by Marcin Pudełek <marcin@pudelek.org.pl>
+ * Date: 09.11.2017
+ * Time: 10:28
+ */
+$ukeFixedPlan = [
+    83 => 'Biała Podlaska',
+    89 => 'Olsztyn',
+    85 => 'Białystok',
+    77 => 'Opole',
+    33 => 'Bielsko Biała',
+    29 => 'Ostrołęka',
+    52 => 'Bydgoszcz',
+    67 => 'Piła',
+    82 => 'Chełm',
+    44 => 'Piotrków Trybunalski',
+    23 => 'Ciechanów',
+    24 => 'Płock',
+    34 => 'Częstochowa',
+    61 => 'Poznań',
+    55 => 'Elbląg',
+    16 => 'Przemyśl',
+    58 => 'Gdańsk',
+    48 => 'Radom',
+    95 => 'Gorzów',
+    17 => 'Rzeszów',
+    75 => 'Jelenia Góra',
+    25 => 'Siedlce',
+    62 => 'Kalisz',
+    43 => 'Sieradz',
+    32 => 'Katowice',
+    46 => 'Skierniewice',
+    41 => 'Kielce',
+    59 => 'Słupsk',
+    63 => 'Konin',
+    87 => 'Suwałki',
+    94 => 'Koszalin',
+    91 => 'Szczecin',
+    12 => 'Kraków',
+    15 => 'Tarnobrzeg',
+    13 => 'Krosno',
+    14 => 'Tarnów',
+    76 => 'Legnica',
+    56 => 'Toruń',
+    65 => 'Leszno',
+    74 => 'Wałbrzych',
+    81 => 'Lublin',
+    22 => 'Warszawa',
+    86 => 'Łomża',
+    54 => 'Włocławek',
+    42 => 'Łódź',
+    71 => 'Wrocław',
+    18 => 'Nowy Sącz',
+    84 => 'Zamość',
+    68 => 'Zielona Góra',
+];
--- src/mrcnpdlk/Validator/Types/Phone.php
+++ src/mrcnpdlk/Validator/Types/Phone.php
@@ -38,9 +38,18 @@ class Phone extends TypeAbstract implements TypeInterface
     const REGEX_VOIP             = '/39\d{7}/';
     const REGEX_UAN              = '/804\d{6}/';
 
+    private $ukeMobilePlan = [];
+    private $ukeFixedPlan  = [];
+
+    /**
+     * Phone constructor.
+     *
+     * @param $checkedValue
+     */
     public function __construct($checkedValue)
     {
         parent::__construct($checkedValue);
+        //pobieramy sam numer krajowy
         $this->checkedValue = substr($this->checkedValue, -9);
     }
 
@@ -69,6 +78,9 @@ class Phone extends TypeAbstract implements TypeInterface
     {
         try {
             static::isValidType($checkedValue, static::TYPE_STRING, true);
+            if (strlen($checkedValue) !== 9 && strlen($checkedValue) !== 11) {
+                throw new \Exception(sprintf('invalid length'));
+            }
             if (!preg_match('/^' . static::COUNTRY_PREFIX . '[\d]{9}$/', $checkedValue)
                 && !preg_match('/^[\d]{9}$/', $checkedValue)) {
                 throw new \Exception('regex error');
@@ -98,30 +110,6 @@ class Phone extends TypeAbstract implements TypeInterface
     public function getInternationalFormat()
     {
         return sprintf('%s%s', static::COUNTRY_PREFIX, $this->get());
-    }
-
-    /**
-     * @return mixed|string
-     */
-    public function getNationalFormat()
-    {
-        return $this->get();
-    }
-
-    /**
-     * @return bool
-     */
-    public function isFixed()
-    {
-        return preg_match(static::REGEX_FIXED, $this->checkedValue) === 1;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isMobile()
-    {
-        return preg_match(static::REGEX_MOBILE, $this->checkedValue) === 1;
     }
 
     /**
@@ -162,5 +150,127 @@ class Phone extends TypeAbstract implements TypeInterface
     public function isVoip()
     {
         return preg_match(static::REGEX_VOIP, $this->checkedValue) === 1;
+    }
+
+    public function getRegion()
+    {
+        $sRegion = null;
+        if ($this->isMobile()) {
+            $nr = $this->getNationalFormat();
+            while (strlen($nr) > 0 || is_null($sRegion)) {
+                if (array_key_exists($nr, $this->getUkeMobilePlan())) {
+                    $sRegion = $this->getUkeMobilePlan()[$nr]['operator'];
+                }
+                $nr = substr($nr, 0, -1);
+            }
+        } elseif ($this->isFixed()) {
+            $sRegion = $this->getUkeFixedPlan()[substr($this->getNationalFormat(),0,2)] ?? null;
+        }
+
+        return $sRegion ?? 'Polska';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isMobile()
+    {
+        return preg_match(static::REGEX_MOBILE, $this->checkedValue) === 1;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getNationalFormat()
+    {
+        return $this->get();
+    }
+
+    /**
+     * @return array
+     */
+    private function getUkeMobilePlan()
+    {
+        if (empty($this->ukeMobilePlan)) {
+            $oXml = new \SimpleXMLElement(file_get_contents(__DIR__ . '/../Databases/T2-PLMN_T9-MVNO.xml'));
+
+            foreach ($oXml->numery->plmn as $oNr) {
+                $t = $this->parseNumberPlan(strval($oNr->numer));
+                foreach ($t as $n) {
+                    $this->ukeMobilePlan[$n] = [
+                        'numer'    => strval($oNr->numer),
+                        'operator' => strval($oNr->operator),
+                        'typ'      => strval($oNr->typ),
+                    ];
+                }
+            }
+        }
+
+        return $this->ukeMobilePlan;
+    }
+
+    /**
+     * @param string $plan
+     *
+     * @return array
+     * @throws \Exception
+     */
+    private function parseNumberPlan(string $plan)
+    {
+        if (preg_match("/(?'prefix'[\d]*)(\((?'suffix'[\d\-,]*)\))?/", strval($plan), $f)) {
+            $answer = [];
+            $prefix = $f['prefix'];
+            $suffix = $f['suffix'] ?? null;
+            $tSuf   = [];
+            if ($suffix) {
+                foreach (explode(',', $suffix) as $digit) {
+                    if (preg_match("/((?'first'[\d]{1})\-(?'last'[\d]{1}))/", $digit, $f)) {
+                        $first = intval($f['first']);
+                        $last  = intval($f['last']);
+                        if ($last === 0) {
+                            $last   = 9;
+                            $tSuf[] = 0;
+                        }
+                        for ($i = $first; $i <= $last; $i++) {
+                            $tSuf[] = $i;
+                        }
+                    } else {
+                        $tSuf[] = intval($digit);
+                    }
+                }
+            }
+            if (empty($tSuf)) {
+                $answer = [$prefix];
+            } else {
+                foreach ($tSuf as $d) {
+                    $answer[] = $prefix . $d;
+                }
+            }
+
+            return $answer;
+        } else {
+            throw new \Exception('regex error');
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFixed()
+    {
+        return preg_match(static::REGEX_FIXED, $this->checkedValue) === 1;
+    }
+
+    /**
+     * @return array
+     */
+    private function getUkeFixedPlan()
+    {
+        if (empty($this->ukeMobilePlan)) {
+            include __DIR__ . '/../Databases/ukeFixedPlan.php';
+            $this->ukeFixedPlan = $ukeFixedPlan;
+        }
+
+        return $this->ukeFixedPlan;
     }
 }
